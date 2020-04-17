522b7f71396e5307112bf2168d35a28bd9777934
mandi <pied.rose@gmail.com>
2020-04-18 00:12:10 +0100

    updated

--- src/modules/pubsub.ts
+++ src/modules/pubsub.ts
@@ -1,23 +1,18 @@
-export function pubsub() {
-  return {
-    subs: new Map(),
-    publish(event: string, data: any): void {
-      console.log(123);
-      if (!this.subs[event]) return;
-      this.subs[event].forEach((subCallback: Function) => {
-        subCallback({ subEvent: event, ...data });
-      });
-      // this.subs[event].forEach((subCallback: Function) => {
-      //   subCallback({ subEvent: event, ...data });
-      // });
-    },
-    subscribe(event: string, callback: Function) {
-      if (!this.subs[event]) this.subs[event] = [];
-      const index = this.subs[event].push(callback) - 1;
-      return { unsubscribe: () => this.subs[event].splice(index, 1) };
-    }
-  };
-}
+const pubsub = {
+  subs: new Map(),
+  publish(event: string, data: any): void {
+    if (!this.subs[event]) return;
+    this.subs[event].forEach(subCallback => subCallback(data));
+  },
+  subscribe(event: string, callback: Function) {
+    if (!this.subs[event]) this.subs[event] = [];
+    const index = this.subs[event].push(callback) - 1;
+    return { unsubscribe: () => this.subs[event].splice(index, 1) };
+  }
+};
+
+export default pubsub;
+
 
 /**usage example
 const subscriber1 = pubSub.subscribe("anEvent", data => {
